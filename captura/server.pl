e1f1aaa2048f4a6e00903c432a234975deac23ac
amijaresp@gmail.com <amijaresp@gmail.com@5a71b732-27f4-aea8-0850-b5c3daef03af>
2013-03-21 18:56:19 +0000

    RC1

--- captura/server.pl
+++ captura/server.pl
@@ -5,6 +5,7 @@ use warnings;
 use Video::Capture::V4l;
 use IO::Socket::UNIX qw( SOCK_STREAM SOMAXCONN );
 use Imager;
+use DigiDB;
 
 my $frame;
 
@@ -16,6 +17,8 @@ my $grab1 = new Video::Capture::V4l('/dev/video1')
 
 my $grab2 = new Video::Capture::V4l('/dev/video2')
     or die "No pude abrir dispositivo $!";
+
+my $obj = DigiDB->new;
 
 my $channel0 = $grab0->channel(0); #1 is composite 0 is for tuner 
 $channel0->norm(1);
@@ -46,27 +49,37 @@ my $socket = $listner->accept()
 
 chomp( my $line = <$socket> );
 
+my $caja = $line;
+
 if ($line) {
     
     print "Client Sez: $line\n";
     
     for my $f (0, 1) {
-	$frame = $grab0->capture(1 - $f, 640, 480);
+	my $frame0 = $grab0->capture(1 - $f, 640, 480);
 	$grab0->sync($f) or die "unable to sync";
     }
-    &guarda('0-' . $line);
+#    &guarda('0-' . $line);
+
+    my $pic0 = &jpeg_roted($frame0);
 
     for my $f (0, 1) {
-	$frame = $grab1->capture(1 - $f, 640, 480);
+	my $frame1 = $grab1->capture(1 - $f, 640, 480);
 	$grab1->sync($f) or die "unable to sync";
     }
-    &guarda('1-' . $line);
+#    &guarda('1-' . $line);
+
+    my $pic1 = &jpeg($frame1);
 
     for my $f (0, 1) {
-	$frame = $grab2->capture(1 - $f, 640, 480);
+	my $frame2 = $grab2->capture(1 - $f, 640, 480);
 	$grab2->sync($f) or die "unable to sync";
     }
-    &guarda('2-' . $line);
+#    &guarda('2-' . $line);
+
+    my $pic2 = &jpeg($frame2);
+
+    $obj->save2pg($pic0, $pic1, $pic2, $caja);
 
     
 }
@@ -74,14 +87,57 @@ if ($line) {
 }
 
 
-sub guarda {
-    my $num = shift;
+# sub guarda {
+#     my $num = shift;
+#     my $temp;
+# # save $fr now, as it contains the raw BGR data 
+#     open (JP, '>', \$temp) or die $!;
+#     print JP "P6\n640 480\n255\n";    #header 
+#     $frame = reverse $frame;
+#     print JP $frame;
+#     close JP;
+    
+#     my $img = Imager->new();
+#     $img->read( data => $temp, type => 'pnm' )
+# 	or warn $img->errstr();
+#     $img->flip( dir => "hv" );
+#     my $img2 = $img->rotate(right=>90);
+#     $img2->write( data => \$temp, type => 'jpeg' )
+# 	or warn $img->errstr;   
+#     open JP, '>', "~/file$num.jpg" or die $!;
+#     print JP $temp;
+#     close JP;
+# }
+
+sub jpeg {
+
+    my $raw = shift;
     my $temp;
 # save $fr now, as it contains the raw BGR data 
     open (JP, '>', \$temp) or die $!;
     print JP "P6\n640 480\n255\n";    #header 
-    $frame = reverse $frame;
-    print JP $frame;
+    $raw = reverse $raw;
+    print JP $raw;
+    close JP;
+    
+    my $img = Imager->new();
+    $img->read( data => $temp, type => 'pnm' )
+	or warn $img->errstr();
+    $img->flip( dir => "hv" );
+    $img->write( data => \$temp, type => 'jpeg' )
+	or warn $img->errstr;
+    return $temp;
+}
+
+sub jpeg_roted {
+
+    my $raw = shift;
+    my $temp;
+# save $fr now, as it contains the raw BGR data 
+    open (JP, '>', \$temp) or die $!;
+    print JP "P6\n640 480\n255\n";    #header 
+    $raw = reverse $raw;
+    print JP $raw;
     close JP;
     
     my $img = Imager->new();
@@ -91,7 +147,5 @@ sub guarda {
     my $img2 = $img->rotate(right=>90);
     $img2->write( data => \$temp, type => 'jpeg' )
 	or warn $img->errstr;   
-    open JP, '>', "~/file$num.jpg" or die $!;
-    print JP $temp;
-    close JP;
+    return $temp;
 }
